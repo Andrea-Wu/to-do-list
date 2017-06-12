552ad32816e71f612b8ed6ac42664a15fbd5c6cd
Andrea Wu <andrea.yh.wu@gmail.com>
2017-06-12 19:39:50 -0400

    Create todoScript.js

--- todoScript.js
+++ todoScript.js
@@ -1,4 +1,4 @@
-var i = 0; //this variable will become a class name that connects all the components of one to-do item
+var i = 0; //this integer will become a class name that connects all the components of one to-do item, for easy deletion
 function display(){
 	var s = document.createElement("span"); 
 	var element = document.getElementById("myInput").value;
@@ -6,9 +6,8 @@ function display(){
 	console.log("hi");
 	s.appendChild(node); //creates text element that uses input from text box
 
-	var iString = i.toString();
-	s.className += iString;
-				//assigns a numbered class to the text item
+	var iString = i.toString(); //turns integer into string for class name
+	s.className += iString; //assigns a class to the span element
 
 	
 
@@ -16,13 +15,13 @@ function display(){
 	var dots = document.createTextNode("........");
 	sp.setAttribute("class", iString);
 	sp.className += " invisible";
-	sp.appendChild(dots);
+	sp.appendChild(dots);			//the invisible (aka white) dots make the list easier to read
 
 	var span = document.createElement("span");
 	var close = document.createTextNode("x");
 	span.setAttribute("class", iString);
 	span.setAttribute("onClick", "hide(this.className);");
-	span.appendChild(close);		//creates "close" button
+	span.appendChild(close);		//it's an "x" that deletes the element when clicked
 
 	i++;
 	//assigns same numbered class to "close" button and increments value
@@ -31,17 +30,15 @@ function display(){
 	br.setAttribute("class", iString);
 
 	document.getElementById("start").appendChild(s); //displays text element
-	document.getElementById("start").appendChild(sp); //invisible dots
-	document.getElementById("start").appendChild(span); //x
-	document.getElementById("start").appendChild(br); //break
+	document.getElementById("start").appendChild(sp); //displays invisible dots
+	document.getElementById("start").appendChild(span); // displays x
+	document.getElementById("start").appendChild(br); //break, aka new line
 
-	document.getElementById("myInput").value = "";
+	document.getElementById("myInput").value = ""; //resets value inside text input box
 	
 }
 
 function hide(class_str){
-	console.log("x");
 	var str = ".".concat(class_str);
-	console.log(str);
 	$(str).remove();
 }
